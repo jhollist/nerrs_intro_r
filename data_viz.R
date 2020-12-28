63d6108860adb0cfa5f5c4b77cc578577bdc945c
Hollister <Hollister.Jeff@epa.gov>
2020-12-28 14:44:24 -0500

    auto commit

--- data_viz.R
+++ data_viz.R
@@ -5,5 +5,6 @@ library(palmerpenguins)
 penguin_gg <- ggplot(data = penguins, aes(x = bill_length_mm, y = bill_depth_mm)) +
   geom_point(aes(color = species, shape = species), size = 2) +
   labs(x = "Bill Length (mm)", y = "Bill Depth (mm)",
-       title = "Association Between Penguin Bill Measurements")
+       title = "Association Between Penguin Bill Measurements") +
+  geom_smooth(method = "loess")
 penguin_gg
