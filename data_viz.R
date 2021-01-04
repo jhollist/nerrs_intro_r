291a832d257431889de5723b0016872e96d69ccf
Hollister <Hollister.Jeff@epa.gov>
2021-01-04 14:34:26 -0500

    auto commit

--- data_viz.R
+++ data_viz.R
@@ -9,7 +9,7 @@ penguin_gg <- ggplot(data = penguins, aes(x = bill_length_mm, y = bill_depth_mm)
        title = "Association Between Penguin Bill Measurements") +
   geom_smooth(aes(group = species), method = "lm") +
   theme_minimal() + 
-  scale_color_manual(values = c("yellow", "green", "black"))
+  scale_color_manual(values = c("#ef8a62", "#f7f7f7", "#67a9cf"))
 penguin_gg
 
 
