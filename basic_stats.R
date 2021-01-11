426fc3cc075aa00f2b8f076237786bee30cb5b17
Hollister <Hollister.Jeff@epa.gov>
2021-01-11 14:16:10 -0500

    auto commit

--- basic_stats.R
+++ basic_stats.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(readr)
 library(tidyr)
 
+# Read and clean NLA data
 nla_wq_all <- read_csv("https://www.epa.gov/sites/production/files/2014-10/nla2007_chemical_conditionestimates_20091123.csv")
 
 nla_wq <- nla_wq_all %>%
@@ -12,5 +13,19 @@ nla_wq <- nla_wq_all %>%
          visit_no == 1) %>%
   select(site_id, st, epa_reg, wsa_eco9, ptl, ntl, turb, chla, doc)
 
+# Read and clean NLA sites
 nla_sites <- read_csv("https://www.epa.gov/sites/production/files/2014-01/nla2007_sampledlakeinformation_20091113.csv")
+
+nla_sites <- nla_sites %>%
+  filter(VISIT_NO == 1) %>%
+  select(SITE_ID, STATE_NAME, CNTYNAME, LAKE_ORIGIN, RT_NLA) %>%
+  rename_all(tolower) %>% #making all names lower case because they are a mess!
+  mutate_if(is.character, tolower)
+
+# Join data and sites
+nla <- left_join(nla_sites, nla_wq, by = "site_id") %>%
+  filter(!is.na(ntl),
+         !is.na(chla),
+         !is.na(ptl))
+
 
