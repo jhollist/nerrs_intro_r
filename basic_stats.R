fefdb26035caccdccd963b552f5c9b7eb5b27190
Hollister <Hollister.Jeff@epa.gov>
2021-01-11 14:52:01 -0500

    auto commit

--- basic_stats.R
+++ basic_stats.R
@@ -66,3 +66,5 @@ TukeyHSD(nla_anova)
 cor(log1p(nla$ptl), log1p(nla$chla), method = "pearson")
 cor.test(log1p(nla$ptl), log1p(nla$chla), method = "pearson")
 
+ptl_chla_lm <- lm(log1p(chla) ~ log1p(ptl) + log1p(ntl) + log1p(ptl):log1p(ntl), data = nla)
+
