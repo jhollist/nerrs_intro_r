1cc26cc525c17bc3248fb32206c9b94eb568f331
Hollister <Hollister.Jeff@epa.gov>
2021-01-11 14:52:37 -0500

    auto commit

--- basic_stats.R
+++ basic_stats.R
@@ -67,4 +67,4 @@ cor(log1p(nla$ptl), log1p(nla$chla), method = "pearson")
 cor.test(log1p(nla$ptl), log1p(nla$chla), method = "pearson")
 
 ptl_chla_lm <- lm(log1p(chla) ~ log1p(ptl) + log1p(ntl) + log1p(ptl):log1p(ntl), data = nla)
-
+summary(ptl_chla_lm)
