c5fbe95ebf48d1e66b87c98c9c8fec505f3134c7
Hollister <Hollister.Jeff@epa.gov>
2021-01-11 14:08:45 -0500

    auto commit

--- basic_stats.R
+++ basic_stats.R
@@ -1,1 +1,13 @@
 # Code for basic stats lesson
+library(dplyr)
+library(readr)
+library(tidyr)
+
+nla_wq_all <- read_csv("https://www.epa.gov/sites/production/files/2014-10/nla2007_chemical_conditionestimates_20091123.csv")
+
+nla_wq <- nla_wq_all %>%
+  rename_all(tolower) %>% #making all names lower case beucase they are a mess!
+  mutate_if(is.character, tolower) %>%
+  filter(site_type == "prob_lake",
+         visit_no == 1) %>%
+  select(site_id, st, epa_reg, wsa_eco9, ptl, ntl, turb, chla, doc)
