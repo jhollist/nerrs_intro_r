4e2a1e690429f0da297aee424a1c8fbea49682bf
Hollister <Hollister.Jeff@epa.gov>
2021-01-25 14:30:42 -0500

    auto commit

--- space_and_time.R
+++ space_and_time.R
@@ -46,6 +46,6 @@ ne_nerrs_wq_no_time <- ne_nerrs_wq_no_time[1:2000,]
 
 ne_nerrs_wq_time <- ne_nerrs_wq_no_time %>%
   mutate(date_time = ymd_hms(bad_time),
-         roll_mean_temp, rollmean(temp,4,))
+         roll_mean_temp = rollmean(temp,4,na.fill = TRUE))
 
 
